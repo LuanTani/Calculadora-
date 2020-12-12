b457c923019b044d5efa03aaeb7550bfdcecad9e
unknown <luanpaivatani@gmail.com>
2020-12-12 10:14:22 -0300

    Altera página inicial do repositório escolhas de IF

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string escolha, sair;
+
+            try
+            {
+                do
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("<------Calculadora------>\n");
+
+                    Console.WriteLine("<------Operações------>");
+                    Console.WriteLine("Somar: +");
+                    Console.WriteLine("Subtrair: -");
+                    Console.WriteLine("Multiplicar: *");
+                    Console.WriteLine("Dividir: /");
+                    Console.WriteLine("Porcentagem: P");
+                    Console.WriteLine("Radiciação (Raiz Quadrada): R");
+                    Console.WriteLine("Exponencial: EX");
+                    Console.WriteLine("Creditos: c\n");
+                    Console.Write("Escolha uma das Opções Acima: ");
+                    escolha = Console.ReadLine().ToUpper();
+
+                    if (escolha == "+")
+                    {
+                        soma();
+                    }
+
+                    if (escolha == "-")
+                    {
+                        subtracao();
+                    }
+
+                    if (escolha == "*")
+                    {
+                        multiplicacao();
+                    }
+
+                    if (escolha == "/")
+                    {
+                        divisao();
+                    }
+
+                    if (escolha == "P")
+                    {
+                        Porcentagem();
+                    }
+
+                    if (escolha == "R")
+                    {
+                        raiz();
+                    }
+                    if (escolha == "EX")
+                    {
+                        Exponencial();
+                    }
+
+                    if (escolha == "C")
+                     {
+                        Creditos();
+                    }
+                     
+                    Console.Write("Deseja Sair? S/N: ");
+                    sair = Console.ReadLine().ToUpper();
--- README.md
+++ README.md
@@ -8,5 +8,33 @@ Soma
 Subtração
 Multiplicação
 Divisão
-Exponenciação
+Porcentagem 
 Radiciação
+Exponencial 
+Creditos 
+
+## _Screenshot_
+
+--- colar foto ---
+
+## _Download_
+
+Baixe o arquivo. Descompacte onde desejar.
+
+[🔥 Download  do arquivo .zip](link para o .zip)
+
+```
+---- Comando Executar Programa ----
+```
+
+Ou, se estiver no Windowns, pode dar  um duplo clique no icone do programa.
+
+## Agradecimentos
+
+- [Etec Adolpho Berezin](https://www.vestibulinhoetec.com.br)
+- [Prof. Ermogenes](https://github.com/ermogenes) 
+- [Prof. Diego Neri](https://github.com/diegoneri)
+
+
+Todos os Direitos reservados PIRATARIA È CRIME!!!
+
