ae272e71af0dbe3c0902048cddcdc608c18f8696
unknown <luanpaivatani@gmail.com>
2020-12-12 14:50:35 -0300

    Continuação do código correção de erros

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Calculadora
                 {
                     Console.Clear();
 
-                    Console.WriteLine("<------Calculadora------>\n");
+                    Console.WriteLine("<----Calculadora One Piece---->\n");
 
                     Console.WriteLine("<------Operações------>");
                     Console.WriteLine("Somar: +");
@@ -24,8 +24,8 @@ namespace Calculadora
                     Console.WriteLine("Porcentagem: P");
                     Console.WriteLine("Radiciação (Raiz Quadrada): R");
                     Console.WriteLine("Exponencial: EX");
-                    Console.WriteLine("Creditos: c\n");
-                    Console.Write("Escolha uma das Opções Acima: ");
+                    Console.WriteLine("Créditos: c\n");
+                    Console.Write("Escolha uma das opções acima: ");
                     escolha = Console.ReadLine().ToUpper();
 
                     if (escolha == "+")
@@ -69,3 +69,90 @@ namespace Calculadora
                      
                     Console.Write("Deseja Sair? S/N: ");
                     sair = Console.ReadLine().ToUpper();
+                }
+                while (sair != "S" && sair != "SIM");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Não se pode dividir por 0.");
+                Console.WriteLine("Aperte Enter para Sair!");
+                Console.ReadKey();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Numero inexistente.");
+                Console.WriteLine("Aperte Enter para Sair!");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Número em formato inválido.");
+                Console.WriteLine("Aperte Enter para Sair!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(); 
+                Console.WriteLine($"Erro genérico: {ex.Message}");
+                Console.WriteLine("Aperte Enter para Sair!");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("Obrigado por Ultilizar");
+        }
+        static void soma()
+        {
+            double x1, x2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1º Valor: ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º Valor: ");
+            x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = x1 + x2;
+            Console.WriteLine($"O resultado de {x1} + {x2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void subtracao()
+        {
+            double x1, x2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1º Valor: ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º Valor: ");
+            x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = x1 - x2;
+            Console.WriteLine($"O resultado de {x1} - {x2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void multiplicacao()
+        {
+            double x1, x2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1º Valor: ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º Valor: ");
+            x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = x1 * x2;
+            Console.WriteLine($"O resultado de {x1} * {x2} = {resultado}");
+            Console.WriteLine();
+        }
