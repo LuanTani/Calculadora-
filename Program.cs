8fbf017099319bec271c90f78616bb0931295c8d
unknown <luanpaivatani@gmail.com>
2020-12-12 15:03:07 -0300

    Conclusão do código e correção de erros das contas

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Calculadora
                 {
                     Console.Clear();
 
-                    Console.WriteLine("<----Calculadora One Piece---->\n");
+                    Console.WriteLine("*----Calculadora One Piece----*\n");
 
                     Console.WriteLine("<------Operações------>");
                     Console.WriteLine("Somar: +");
@@ -156,3 +156,110 @@ namespace Calculadora
             Console.WriteLine($"O resultado de {x1} * {x2} = {resultado}");
             Console.WriteLine();
         }
+        static void divisao()
+        {
+            double x1, x2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1º Valor: ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º Valor: ");
+            x2 = double.Parse(Console.ReadLine());
+
+            if (x2 == 0)
+            {
+                throw new DivideByZeroException("Não se pode dividir por 0.");
+            }
+
+            Console.WriteLine();
+            resultado = x1 / x2;
+
+            Console.WriteLine($"O resultado de {x1} / {x2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void Porcentagem()
+        {
+           double x1, y1, x2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite a sua Porcentagem: ");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor: ");
+            y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            x2 = x1  / 100;
+
+            Console.WriteLine();
+            resultado = x2  * y1;
+
+            Console.WriteLine($"O resultado de {x1} % de {y1} é = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void raiz()
+        {
+            double rQ, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o Valor de Raiz Quadrada: ");
+            rQ = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = Math.Sqrt(rQ);
+            Console.WriteLine($"A raiz quadrada de {rQ} é = {Math.Round(resultado, 2)}");
+        }
+
+           static void Exponencial()
+     
+        {
+   double x1, x2, resultado;
+
+           
+            
+            Console.Clear();
+
+           
+            
+            Console.Write("Digite o 1° numero: ");
+            
+            x1 = double.Parse(Console.ReadLine());
+
+ 
+            
+            Console.Write("Digite o 2° numero: ");
+   
+            x2 = double.Parse(Console.ReadLine());
+
+  
+            
+            Console.WriteLine();
+     
+            resultado = Math.Pow(x1,x2);
+
+   
+            
+            Console.WriteLine($"A função de F({x2})a {x1} é = {resultado}");
+   
+            Console.WriteLine();
+      
+        }  
+
+          static void Creditos()
+        {
+                    Console.Clear();
+
+                    Console.WriteLine("Progamador: Luan Tani");
+                    Console.WriteLine("Escola: Etec Adolpho berezin ");
+                    Console.WriteLine("Materia: P.C 1");
+                    Console.WriteLine("Projeto C# Final ");
+                    Console.WriteLine("Obrigado por Testar");
+        }
+    }
+}
